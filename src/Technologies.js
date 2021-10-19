72802134aab154889997c500b734726fccd429b0
artem-viktorovich <artemkavasechko@yandex.ru>
2021-10-19 19:41:55 +0300

    Technologies_ comoinents

---
+++ src/Technologies.js
@@ -0,0 +1,15 @@
+let Technologies = () => {
+	return (
+		<div className="App">
+		<ul>
+			<li>html</li>
+			<li>css</li>
+			<li>js</li>
+			<li>react</li>
+			<li>ку-ку</li>
+		</ul>
+	</div>
+	);
+}
+
+export default Technologies;
