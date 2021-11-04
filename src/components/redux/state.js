a1435140009bc6d67f53ff254d0d3f0c76a70352
artem-viktorovich <artemkavasechko@yandex.ru>
2021-11-04 15:00:00 +0300

    redux folder

---
+++ src/components/redux/state.js
@@ -0,0 +1,25 @@
+
+let state = {
+	posts: [
+		{ id: 1, message: 'Привет бейби', LikesCount: 22},
+		{ id: 2, message: 'Здоров', LikesCount: 21 },
+		{ id: 2, message: 'Пока', LikesCount: 21 },
+		{ id: 2, message: 'Йоу', LikesCount: 21 }
+	],
+	dialogs: [
+		{ id: 1, name: 'Dimych' },
+		{ id: 2, name: 'Andrew' },
+		{ id: 3, name: 'Sveta' },
+		{ id: 4, name: 'Sasha' },
+		{ id: 5, name: 'Valera' }
+	],
+	messages: [
+		{ id: 1, message: 'Привет' },
+		{ id: 2, message: 'Как дела?' },
+		{ id: 3, message: 'Ты кто?' },
+		{ id: 4, message: 'Пошли гулять' },
+		{ id: 5, message: 'Хай' }
+	]
+}
+
+export default state;
--- src/index.js
+++ src/index.js
@@ -3,30 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
-let posts = [
-	{ id: 1, message: 'Привет бейби', LikesCount: 22},
-	{ id: 2, message: 'Здоров', LikesCount: 21 },
-	{ id: 2, message: 'Пока', LikesCount: 21 },
-	{ id: 2, message: 'Йоу', LikesCount: 21 }
-]
-let dialogs = [
-	{ id: 1, name: 'Dimych' },
-	{ id: 2, name: 'Andrew' },
-	{ id: 3, name: 'Sveta' },
-	{ id: 4, name: 'Sasha' },
-	{ id: 5, name: 'Valera' }
-]
-let messages = [
-	{ id: 1, message: 'Привет' },
-	{ id: 2, message: 'Как дела?' },
-	{ id: 3, message: 'Ты кто?' },
-	{ id: 4, message: 'Пошли гулять' },
-	{ id: 5, message: 'Хай' }
-]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App posts={state} dialogs={state} messages={state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
