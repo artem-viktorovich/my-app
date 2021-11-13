e3c4fdd034f65a3b4c2afd7bc59087aa9045a963
artem-viktorovich <artemkavasechko@yandex.ru>
2021-11-13 21:56:53 +0300

    bind

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 //import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText } from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 
-let rerenderEntureTree = () => {
+let rerenderEntureTree = (state) => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+			<App state={state} addPost={store.addPost.bind(store)} 
+			updateNewPostText={store.updateNewPostText.bind(store)}
+			
+			/>
 		</BrowserRouter>,	document.getElementById('root')
 	);
 }
 
 
 
-rerenderEntureTree(state);
-subscribe(rerenderEntureTree); //отправляем функцию для её вызова коллбек
+rerenderEntureTree(store.getState());
+store.subscribe(rerenderEntureTree); //отправляем функцию для её вызова коллбек
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,55 +1,55 @@
-let rerenderEntureTree = () => {
-	console.log('Kazah');
-}
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 1, message: 'Привет бейби', LikesCount: 22 },
+				{ id: 2, message: 'Здоров', LikesCount: 21 },
+				{ id: 3, message: 'Пока', LikesCount: 21 },
+				{ id: 4, message: 'Йоу', LikesCount: 21 }
+			],
+			newPostText: ''
+		},
+		dialogsPage: {
+			dialogs: [
+				{ id: 1, name: 'Dimych' },
+				{ id: 2, name: 'Andrew' },
+				{ id: 3, name: 'Sveta' },
+				{ id: 4, name: 'Sasha' },
+				{ id: 5, name: 'Valera' }
+			],
+			messages: [
+				{ id: 1, message: 'Привет' },
+				{ id: 2, message: 'Как дела?' },
+				{ id: 3, message: 'Ты кто?' },
+				{ id: 4, message: 'Пошли гулять' },
+				{ id: 5, message: 'Хай' }
+			]
+		}
 
-let state = {
-	profilePage: {
-		posts: [
-			{ id: 1, message: 'Привет бейби', LikesCount: 22 },
-			{ id: 2, message: 'Здоров', LikesCount: 21 },
-			{ id: 3, message: 'Пока', LikesCount: 21 },
-			{ id: 4, message: 'Йоу', LikesCount: 21 }
-		],
-		newPostText: ''
 	},
-	dialogsPage: {
-		dialogs: [
-			{ id: 1, name: 'Dimych' },
-			{ id: 2, name: 'Andrew' },
-			{ id: 3, name: 'Sveta' },
-			{ id: 4, name: 'Sasha' },
-			{ id: 5, name: 'Valera' }
-		],
-		messages: [
-			{ id: 1, message: 'Привет' },
-			{ id: 2, message: 'Как дела?' },
-			{ id: 3, message: 'Ты кто?' },
-			{ id: 4, message: 'Пошли гулять' },
-			{ id: 5, message: 'Хай' }
-		]
+	getState() {
+		return this._state;
+	},
+	_callSubscriber() {
+		console.log('Kazah');
+	},
+	addPost() {
+		let newPost = {
+			id: 5,
+			message: this._state.profilePage.newPostText,
+			LikesCount: 0
+		};
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = '';
+		this._callSubscriber(this._state); //запускает перерисовку BLL
+	},
+	updateNewPostText(newText) {
+		this._state.profilePage.newPostText = newText;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
 	}
-
-}
-window.state = state;
-export const addPost = () => {
-	let newPost = {
-		id: 5,
-		message: state.profilePage.newPostText,
-		LikesCount: 0
-	};
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = '';
-	rerenderEntureTree(state); //запускает перерисовку BLL
 }
 
-export const updateNewPostText = (newText) => {
-	state.profilePage.newPostText = newText;
-	rerenderEntureTree(state);
-}
-
-
-export const subscribe = (observer) => {
-	rerenderEntureTree = observer;
-}
-
-export default state;
+export default store;
